43da7c826832cf63ba3064a04ec5d3b19a0962f0
Insanitykid2879 <131005583+Insanitykid2879@users.noreply.github.com>
2025-05-09 11:54:08 -0400

    Delete Capstone directory

--- Capstone/Games/Card_game/card.py
+++
@@ -1,9 +0,0 @@
-# card.py
-
-class Card:
-    def __init__(self, suit, value):
-        self.suit = suit
-        self.value = value
-    
-    def __str__(self):
-        return f"{self.value} of {self.suit}"
